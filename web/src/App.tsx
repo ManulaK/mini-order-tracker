84e28b6aab099ebed495fae1e9b8e729fe612129
ManulaK <manulakavinda.dev@gmail.com>
2026-07-15 02:02:04 +0530

    refactor: remove AppHeader component from App and delete its file

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,9 @@
 import { OrdersPage } from "./modules/orders";
-import { AppHeader } from "./shared/components/AppHeader";
 
 function App() {
 	return (
 		<main className="min-h-screen bg-stone-50 text-zinc-950">
 			<section className="mx-auto flex min-h-screen w-full max-w-[1440px] flex-col px-6 py-5 sm:px-8 lg:px-12 xl:px-16">
-				<AppHeader />
 				<OrdersPage />
 			</section>
 		</main>
